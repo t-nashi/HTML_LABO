9054b4917b5d77a8318f9c3cacf1ad9c72b21850
t-nashi <t_19nashi@yahoo.co.jp>
2023-02-17 15:39:37 +0900

    テンプレート追加

---
+++ 04_jQuery/js/main.js
@@ -0,0 +1,4 @@
+$(window).on('load', function() {
+  alert('html読み込み後のアラートテキスト');
+});
+// chromeコンソールに「$.fn.jquery;」でjquery確認可能
---
+++ 05_Vue.js/js/main.js
@@ -0,0 +1,34 @@
+// // ・ES5までの変数宣言は「var」のみ
+// // ・letは「再宣言するとエラーが出る」
+// // ・constは「定数としてあつかえる宣言」
+
+// // js読み込みと同時に実行
+// (function () {
+//   console.log("js読み込みと同時に実行");
+// }());
+
+// // html内容読み込み完了後に実行
+// window.onload = function() {
+//   console.log("html内容読み込み完了後に実行");
+//   const e = document.querySelector(".title");
+//   // console.log(e.innerHTML);
+//   console.log(e.textContent);
+// }
+
+
+// 2.x
+// new Vue({
+//   el: '#app',
+//   data: {
+//     activetab: 1
+//   },
+// });
+
+// 3.x
+Vue.createApp({
+  data() {
+    return {
+      activetab: 1
+    }
+  },
+}).mount('#app');
